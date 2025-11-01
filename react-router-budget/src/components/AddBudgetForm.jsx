4d025545451f9a325b5443ee51fc13deb9382657
Mateusz Okoń <mateusz.okon04@gmail.com>
2025-11-01 20:25:49 +0100

    new Form

---
+++ react-router-budget/src/components/AddBudgetForm.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {Form} from "react-router-dom"
+import {CurrencyEuroIcon} from '@heroicons/react/24/solid'
+
+export const AddBudgetForm = () => {
+  return (
+    <div className='form-wrapper'>
+        <h2 className='h3'>
+            Stwórz nowy budżet
+        </h2>
+        <Form
+        method='post'
+        className='grid-sm'>
+        <div className='grid-xs'>
+            <label htmlFor='newBudget'>Nazwa Nowego Budżetu</label>
+            <input type='text'
+             name='newBudget'
+              id='newBudget'
+              placeholder='np. Kosmetyki'
+              required
+              >
+              </input>
+        </div>
+        <div className="grid-xs">
+            <label htmlFor='newBudgetAmount'>
+                Kwota
+            </label>
+            <input
+            type='number'
+            step='0.01'
+            name='newBudgetAmount'
+            id='newBudgetAmount'
+            placeholder='np. 350zł'
+            required
+            inputMode='decimal'
+            >
+            </input>
+        </div>
+        <button type='submit' className='btn btn--dark'>
+            <span>Dodaj</span>
+            <CurrencyEuroIcon width={20}/>
+        </button>
+        </Form>
+    </div>
+  )
+}
--- react-router-budget/src/pages/Dashboard.jsx
+++ react-router-budget/src/pages/Dashboard.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { fetchData } from '../helpers'
 import { useLoaderData } from 'react-router-dom';
+import { AddBudgetForm } from '../components/AddBudgetForm';
 
 export function dashboardLoader(){
     const userName = fetchData("userName");
-    return { userName }
+    const budget = fetchData("budget");
+    return { userName, budget }
 }
 
 const Dashboard = () => {
     const { userName } = useLoaderData()
   return (
     <>
-      {userName ? <p>{userName}</p> : 'Zaloguj się'}
+      {userName ? (
+      <div className="dashboard">
+        <h1>Witaj spowrotem, <span className='accent'>{userName}</span></h1>
+        <div className="grid-sm">
+          {/* {budget ? () : ()} */}
+          <div className="grid-lg">
+            <div className="flex-lg">
+              <AddBudgetForm/>
+            </div>
+          </div>
+        </div>
+      </div>
+      ) : 'Zaloguj się'}
     </>
   )
 }
